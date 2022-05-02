df6244d35159ee360840c08257935312302e59d2
Drew Showalter <10665739+one19@users.noreply.github.com>
2022-05-02 18:23:49 +1000

    :sparkles: allow big queries and test it.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { DynamoDB } from 'aws-sdk';
 import { DataSource } from 'apollo-datasource';
+import { AttributeMap, ScanOutput } from 'aws-sdk/clients/dynamodb';
 import { createUpdateExpression } from './createUpdateExpression';
 import { createFilterExpression, Query as _Query } from './createFilterExpression';
 import { createExpressions } from './createExpressions';
+
+const CHUNK = 100; // max array properties in dynamo queries/scans
 
 export type ConstructionProps = {
   getId: () => string;
@@ -21,7 +24,7 @@ export type ItemsResponse<T> = {
   items: T[];
 };
 export type ContextOptions = {
-  hashKey: DynamoDB.Key;
+  hashKey: any;
   ConditionExpression?: string;
   withMetadata?: boolean;
 };
@@ -135,17 +138,69 @@ export default class DocDatabase<T> extends DataSource {
     };
   }
 
+  async _batchScan(query: Query): Promise<ScanOutput> {
+    const [longKey, longQuery] = Object.entries(query).find(
+      ([, value]) => Array.isArray(value) && value.length > CHUNK
+    ) as [string, any[]];
+
+    const chunkedQuery = longQuery.reduce(
+      (chunks: any[][], item: any, i: number) => {
+        chunks[Math.floor(i / CHUNK)].push(item);
+        return chunks;
+      },
+      Array.from({ length: Math.ceil(longQuery.length / CHUNK) }, () => [])
+    );
+
+    const results = await Promise.all(
+      chunkedQuery.map((val) =>
+        this.client
+          .scan({
+            ...createFilterExpression({ ...query, [longKey]: val }),
+            ReturnConsumedCapacity: 'TOTAL',
+            TableName: this.tableName,
+          })
+          .promise()
+      )
+    );
+
+    const items = results.map((result) => result.Items).flat() as AttributeMap[];
+
+    return {
+      ConsumedCapacity: {
+        CapacityUnits: results.reduce(
+          (sum, result) => (result?.ConsumedCapacity?.CapacityUnits ?? 0) + sum,
+          0
+        ),
+      },
+      LastEvaluatedKey: results[results.length - 1].LastEvaluatedKey,
+      Items: items,
+      Count: items.length,
+    };
+  }
+
   async query(
     query: Query,
     { withMetadata }: Partial<ContextOptions> = {}
   ): Promise<T[] | ItemsResponse<T>> {
-    const result = await this.client
-      .scan({
-        ...createFilterExpression({ ...query }),
-        ReturnConsumedCapacity: 'TOTAL',
-        TableName: this.tableName,
-      })
-      .promise();
+    const longArrayQueries = Object.values(query).filter(
+      (e) => Array.isArray(e) && e.length > CHUNK
+    );
+    if (longArrayQueries.length > 1) {
+      // you could, in fact, create an overlapping matrix that would get all elements
+      // but it scales into _unusable_ too fast. if you want 2+ array matchers with
+      // 100+ elements in each parameter, it's very like an architecture mistake
+      throw new Error('Too many long IN statements to resolve well.');
+    }
+
+    const result = await (longArrayQueries.length === 1
+      ? this._batchScan(query)
+      : this.client
+          .scan({
+            ...createFilterExpression({ ...query }),
+            ReturnConsumedCapacity: 'TOTAL',
+            TableName: this.tableName,
+          })
+          .promise());
 
     if (!withMetadata) return result.Items as T[];
 
